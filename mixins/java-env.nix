75a5aca8e74f44125361ae10621978ff173f7371
lbodor <lazar.bodor@ga.gov.au>
2016-07-11 12:30:12 +1000

    Set JAVA_HOME

--- mixins/java-env.nix
+++ mixins/java-env.nix
@@ -9,4 +9,8 @@
     # TODO: When can we start using SSL with GA's external Nexus?
     mvn = "mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true";
   };
+
+  environment.extraInit = ''
+    export JAVA_HOME=$(dirname $(dirname $(readlink -e $(which java))))
+  '';
 }
